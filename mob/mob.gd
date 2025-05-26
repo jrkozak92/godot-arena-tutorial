01908bb4fd9468ff71a201244983bff25b3c2456
Joey Kozak <jrkozak92@gmail.com>
2025-05-25 19:19:59 -0500

    Mob faces player during pursuit

--- mob/mob.gd
+++ mob/mob.gd
@@ -1,7 +1,15 @@
 extends RigidBody3D
 
-@onready var bat_model = %bat_model
+var speed = randf_range(2.0, 4.0)
 
+@onready var bat_model = %bat_model
+@onready var player = get_node("/root/Game/Player")
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	direction.y = 0.0
+	linear_velocity = direction * speed
+	bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
 
 func take_damage():
 	bat_model.hurt()
